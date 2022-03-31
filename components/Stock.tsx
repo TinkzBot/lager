4eae7650acd520912747260995faf79660f81ee3
TinkzBot <zazi21@student.bth.se>
2022-03-31 20:34:29 +0200

    lager1 done

--- components/Stock.tsx
+++ components/Stock.tsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import config from "../config/config.json";
 
 function StockList() {
-	const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState<any[]>([]);
 
-    useEffect(() => {
+	useEffect(() => {
 		fetch(`${config.base_url}/products?api_key=${config.api_key}`)
 			.then((response) => response.json())
 			.then((result) => setProducts(result.data));
