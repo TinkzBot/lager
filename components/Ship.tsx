44e1d0f0400c04a2768c32107f13e074dd91be7c
TinkzBot <zakaria.ziadi92@gmail.com>
2022-05-31 15:18:25 +0200

    Fix Kmom05

--- components/Ship.tsx
+++ components/Ship.tsx
@@ -1,18 +1,20 @@
-import { useLinkProps } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ProgressViewIOSComponent } from "react-native";
+import { useLinkProps } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ProgressViewIOSComponent } from 'react-native';
 import { View, Text, Button } from "react-native";
 
-import ShipList from "./ShipList";
-import ShipOrder from "./ShipOrder";
+import ShipList from './ShipList';
+import ShipOrder from './ShipOrder';
 
 const Stack = createNativeStackNavigator();
 
 export default function Pick(props) {
-	return (
-		<Stack.Navigator initialRouteName="List">
-			<Stack.Screen name="List" component={ShipList} />
-			<Stack.Screen name="Details" component={ShipOrder} />
-		</Stack.Navigator>
-	);
+    return (
+        <Stack.Navigator initialRouteName="List">
+            <Stack.Screen name="List" component={ShipList} />
+            <Stack.Screen name="Details">
+                {(screenProps) => <ShipOrder {...screenProps} setProducts={props.setProducts} />}
+            </Stack.Screen>
+        </Stack.Navigator>
+    );
 }
--- components/ShipList.tsx
+++ components/ShipList.tsx
@@ -1,46 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { View, Text, Button } from "react-native";
 import { Base, Typography } from "../styles";
 
 import orderModel from "../models/orders";
 import productModel from "../models/products";
 
-export default function ShipList({ route, navigation }) {
-	const { reload } = route.params || false;
-	const [allOrders, setAllOrders] = useState([]);
+export default function ShipList({route, navigation }) {
+    const { reload } = route.params || false;
+    const [allOrders, setAllOrders] = useState([]);
 
-	if (reload) {
-		reloadOrders();
-	}
+    if (reload) {
+        reloadOrders();
+    }
 
-	async function reloadOrders() {
-		setAllOrders(await orderModel.getOrders());
-	}
+    async function reloadOrders() {
+        setAllOrders(await orderModel.getOrders());
+    }
 
-	useEffect(() => {
-		reloadOrders();
-	}, []);
+    useEffect(() => {
+        reloadOrders();
+    }, []);
 
-	const listOfOrders = allOrders
-		.filter((order) => order.status === "Ny")
-		.map((order, index) => {
-			return (
-				<Button
-					title={order.name}
-					key={index}
-					onPress={() => {
-						navigation.navigate("Details", {
-							order: order,
-						});
-					}}
-				/>
-			);
-		});
+    const listOfOrders = allOrders
+        .filter(order => order.status === "Packad")
+        .map((order, index) => {
+            return <Button
+                title={order.name}
+                key={index}
+                onPress={() => {
+                    navigation.navigate('Details', {
+                        order: order
+                    });
+                }}
+            />
+        });
 
-	return (
-		<View>
-			<Text style={Typography.header3}>Ordrar redo att levereras</Text>
-			{listOfOrders}
-		</View>
-	);
+    return (
+        <View>
+            <Text style={Typography.header3}>Ordrar redo att levereras</Text>
+            {listOfOrders}
+        </View>
+    );
 }
